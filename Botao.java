c4fd38ad66f4c6fc7ba8eae04b596ee6d184d42c
felipe quentino <100160215+felipequentino@users.noreply.github.com>
2023-02-14 09:41:26 -0300

    Add files via upload

---
+++ Botao.java
@@ -0,0 +1,21 @@
+package io.github.felipequentino.calc.visao;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+@SuppressWarnings("serial")
+public class Botao extends JButton{
+
+	public Botao(String texto, Color cor) {
+		setText(texto);
+		setFont(new Font("courier", Font.ROMAN_BASELINE, 25));
+		setOpaque(true);
+		setBackground(cor);
+		setForeground(Color.WHITE);
+		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+	}
+
+}
---
+++ MemoriaObservador.java
@@ -0,0 +1,6 @@
+package io.github.felipequentino.calc.modelo;
+
+public interface MemoriaObservador {
+
+	public void valorAlterado(String novoValor);
+}
